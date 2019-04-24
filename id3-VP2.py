fe8f36e622a739586d7f749f8c504ccae95292d8
rosinele <rosefelix99@gmail.com>
2019-04-24 00:21:56 -0300

    dicionário de dados

---
+++ id3-VP2.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+
+dados_credito=[
+    {"id": 1, "risco": 'alto', "hc": 'ruim', "divida": 'alta', "garantia": 'nenhuma', "renda": '$0 a $15 mil'},
+    {"id": 2, "risco": 'alto', "hc": 'desconhecida', "divida": 'alta', "garantia": 'nenhuma', "renda": '$15 a $35 mil'},
+    {"id": 3, "risco": 'moderado', "hc": 'desconhecida', "divida": 'baixa', "garantia": 'nenhuma', "renda": '$15 a $35 mil'},
+    {"id": 4, "risco": 'alto', "hc": 'desconhecida', "divida": 'baixa', "garantia": 'nenhuma', "renda": '$0 a $15 mil'},
+    {"id": 5, "risco": 'baixo', "hc": 'desconhecida', "divida": 'baixa', "garantia": 'nenhuma', "renda": 'acima de $35 mil'},
+    {"id": 6, "risco": 'baixo', "hc": 'desconhecida', "divida": 'baixa', "garantia": 'adequada', "renda": 'acima de $35 mil'},
+    {"id": 7, "risco": 'alto', "hc": 'ruim', "divida": 'baixa', "garantia": 'nenhuma', "renda": '$0 a $15 mil'},
+    {"id": 8, "risco": 'moderado', "hc": 'ruim', "divida": 'baixa', "garantia": 'adequada', "renda": 'acima de $35 mil'},
+    {"id": 9, "risco": 'baixo', "hc": 'boa', "divida": 'baixa', "garantia": 'nenhuma', "renda": 'acima de $35 mil'},
+    {"id": 10, "risco": 'baixo', "hc": 'boa', "divida": 'alta', "garantia": 'adequada', "renda": 'acima de $35 mil'},
+    {"id": 11, "risco": 'alto', "hc": 'boa', "divida": 'alta', "garantia": 'nenhuma', "renda": '$0 a $15 mil'},
+    {"id": 12, "risco": 'moderado', "hc": 'boa', "divida": 'alta', "garantia": 'nenhuma', "renda": '$15 a $35 mil'},
+    {"id": 13, "risco": 'baixo', "hc": 'boa', "divida": 'alta', "garantia": 'nenhuma', "renda": 'acima de $35 mil'},
+    {"id": 14, "risco": 'alto', "hc": 'ruim', "divida": 'alta', "garantia": 'nenhuma', "renda": '$15 a $35 mil'},
+]
+
+
+
+    
